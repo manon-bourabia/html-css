983adae816ebfbcb190852aafdebe9d32af6689d
Manondev11 <manon.elm@hotmail.com>
2026-02-03 15:54:26 +0100

    calculatrice

--- JS-FORM-1/index.js
+++ JS-FORM-1/index.js
@@ -1,1 +1,13 @@
-alert(document.getElementById('nom').value + " " + document.getElementById('prenom').value)
+// alert(document.getElementById('nom').value + " " + document.getElementById('prenom').value);
+
+document.getElementById('my-bouton').onclick = function direBonjour(){
+  alert('Hello !');
+}
+// Calculatrice
+document.getElementById('mon-bouton').onclick = function calculer(){
+    let value1 = document.getElementById('premier-nombre').value;
+    let value2 = document.getElementById('deuxieme-nombre').value;
+    let nombre1 = parseFloat(value1);
+    let nombre2 = parseFloat(value2);
+    resultat.value = nombre1 + nombre2; 
+}
